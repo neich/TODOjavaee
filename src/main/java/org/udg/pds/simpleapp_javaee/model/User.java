51e3bdf9779b04740cd375675adc934648e70678
Ignacio Martín <ignacio.martin@udg.edu>
2018-03-06 16:40:31 +0100

    Table chnged to "users" because of problems with PostgreSQL

--- src/main/java/org/udg/pds/simpleapp_javaee/model/User.java
+++ src/main/java/org/udg/pds/simpleapp_javaee/model/User.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-@Entity
+@Entity(name = "users")
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = {"email", "username"}))
 public class User implements Serializable {
   /**
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 -- You can use this file to load seed data into the database using SQL statements
-insert into User (username, email, password) values ('jo', 'jp@hotmail.com', 'jo');
-insert into User (username, email, password) values ('tu', 'tu@hotmail.com', 'tu');
+insert into users (username, email, password) values ('jo', 'jp@hotmail.com', 'jo');
+insert into users (username, email, password) values ('tu', 'tu@hotmail.com', 'tu');
