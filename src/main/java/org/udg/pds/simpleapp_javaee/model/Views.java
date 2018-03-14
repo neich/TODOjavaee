49ff544e889932e9a2365c92c335a570716ef96e
Ignacio Martín <ignacio.martin@udg.edu>
2018-03-14 16:57:08 +0100

    Changed view Complete to UserProfile

--- src/main/java/org/udg/pds/simpleapp_javaee/model/Views.java
+++ src/main/java/org/udg/pds/simpleapp_javaee/model/Views.java
@@ -3,5 +3,5 @@ package org.udg.pds.simpleapp_javaee.model;
 public class Views {
   static public class Public { }
   static public class Private extends Public { }
-  static public class Complete extends Private { }
+  static public class UserProfile extends Private { }
 }
