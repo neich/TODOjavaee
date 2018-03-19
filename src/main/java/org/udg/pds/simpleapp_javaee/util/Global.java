f2e1560da75e10a2121729c82092bb7cc82eeeb9
Ignacio Martín <ignacio.martin@udg.edu>
2018-03-19 21:15:39 +0100

    Add minio support. Add base URL for image REST access

--- src/main/java/org/udg/pds/simpleapp_javaee/util/Global.java
+++ src/main/java/org/udg/pds/simpleapp_javaee/util/Global.java
@@ -1,6 +1,8 @@
 package org.udg.pds.simpleapp_javaee.util;
 
 
+import io.minio.MinioClient;
+
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
@@ -8,13 +10,31 @@ import javax.ejb.Startup;
 @Singleton
 @Startup
 public class Global {
+    private MinioClient minioClient;
 
-    String dataDir;
+    private static String BASE_URL;
 
     @PostConstruct
     void init() {
-        dataDir = System.getProperty("local.data-dir");
+        try {
+            String minioURL = System.getProperty("swarm.project.minio.url");
+            String minioAccessKey = System.getProperty("swarm.project.minio.access-key");
+            String minioSecretKey = System.getProperty("swarm.project.minio.secret-key");
+            minioClient = new MinioClient(minioURL, minioAccessKey, minioSecretKey);
+        } catch (Exception e) {
+        }
+
+        if (System.getProperty("swarm.project.base-url") != null)
+            BASE_URL = System.getProperty("swarm.project.base-url");
+        else {
+            String port = System.getProperty("swarm.http.port") != null ? System.getProperty("swarm.http.port") : "8080";
+            BASE_URL = "http://localhost:" + port;
+        }
     }
 
-    public String getDataDir() { return dataDir; }
+    public MinioClient getMinioClient() {
+        return minioClient;
+    }
+
+    public String getBaseURL() { return BASE_URL; }
 }
