5d31829e5f7c0d9573de6bab52b3c08366258418
Ignacio Martín <ignacio.martin@udg.edu>
2018-03-19 16:42:33 +0100

    Added support for Minio based object storage

--- src/main/java/org/udg/pds/simpleapp_javaee/rest/ImageRESTService.java
+++ src/main/java/org/udg/pds/simpleapp_javaee/rest/ImageRESTService.java
@@ -1,5 +1,8 @@
 package org.udg.pds.simpleapp_javaee.rest;
 
+import com.sun.mail.iap.ByteArray;
+import io.minio.MinioClient;
+import org.apache.commons.io.IOUtils;
 import org.jboss.resteasy.plugins.providers.multipart.InputPart;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 import org.udg.pds.simpleapp_javaee.util.Global;
@@ -12,80 +15,72 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
 import java.io.*;
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Path("/images")
 @RequestScoped
 public class ImageRESTService extends RESTService {
 
-  @Inject
-  Global global;
+    @Inject
+    Global global;
 
-  @POST
-  @Produces(MediaType.APPLICATION_JSON)
-  @Consumes(MediaType.MULTIPART_FORM_DATA)
-  public String upload(@Context HttpServletRequest req,
-                       MultipartFormDataInput input) {
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response upload(@Context HttpServletRequest req,
+                           MultipartFormDataInput input) {
 
-    Map<String, List<InputPart>> formParts = input.getFormDataMap();
+        Map<String, List<InputPart>> formParts = input.getFormDataMap();
+        MinioClient minioClient = global.getMinioClient();
 
-    List<InputPart> inPart = formParts.get("file");
+        List<String> images = new ArrayList<>();
 
-    for (InputPart inputPart : inPart) {
+        for (List<InputPart> inPart : input.getFormDataMap().values())
+            for (InputPart inputPart : inPart) {
 
-      try {
-        // Retrieve headers, read the Content-Disposition header to obtain the original name of the file
-        MultivaluedMap<String, String> headers = inputPart.getHeaders();
-        String fileName = parseFileName(headers);
+                if (!inputPart.getMediaType().getType().equals("image")) continue;
 
-        // Handle the body of that part with an InputStream
-        InputStream istream = inputPart.getBody(InputStream.class, null);
+                try {
+                    // Handle the body of that part with an InputStream
+                    InputStream istream = inputPart.getBody(InputStream.class, null);
+                    String contentType = inputPart.getMediaType().toString();
+                    UUID imgName = UUID.randomUUID();
 
-        fileName = global.getDataDir() + fileName;
+                    String objectName = imgName + "." + inputPart.getMediaType().getSubtype();
+                    // Upload the file to the bucket with putObject
+                    minioClient.putObject("pds",
+                            objectName,
+                            istream,
+                            contentType);
 
-        saveFile(istream, fileName);
+                    images.add(global.getBaseURL() + "/rest/images/" + objectName);
 
-      } catch (Exception e) {
-        throw new WebApplicationException("Error saving file: " + e.getMessage());
-      }
+                } catch (Exception e) {
+                    throw new WebApplicationException("Error saving file: " + e.getMessage());
+                }
+            }
+
+        return buildResponse(images);
     }
 
-    return "{}";
-  }
+    @GET
+    @Path("{filename}")
+    public Response download(@PathParam("filename") String filename) {
 
-  // Parse Content-Disposition header to get the original file name
-  private String parseFileName(MultivaluedMap<String, String> headers) {
-
-    String[] contentDispositionHeader = headers.getFirst("Content-Disposition").split(";");
-
-    for (String name : contentDispositionHeader) {
-
-      if ((name.trim().startsWith("filename"))) {
-
-        String[] tmp = name.split("=");
-
-        String fileName = tmp[1].trim().replaceAll("\"", "");
-
-        return fileName;
-      }
+        MinioClient minioClient = global.getMinioClient();
+        try {
+            InputStream file = minioClient.getObject("pds", filename);
+            return Response.ok(IOUtils.toByteArray(file)).type("image/png").build();
+        } catch (Exception e) {
+            throw new WebApplicationException("Error downloading file: " + e.getMessage());
+        }
     }
-    return "randomName";
-  }
 
-  // save uploaded file to a defined location on the server
-  private void saveFile(InputStream uploadedInputStream,
-                        String serverLocation) throws IOException {
-
-    int read;
-    byte[] bytes = new byte[1024];
-
-    OutputStream outpuStream = new FileOutputStream(new File(serverLocation));
-    while ((read = uploadedInputStream.read(bytes)) != -1) {
-      outpuStream.write(bytes, 0, read);
-    }
-    outpuStream.flush();
-    outpuStream.close();
-  }
 }
